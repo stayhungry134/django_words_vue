69a71e8896a9aeb612e4e492d10db78588e5c91b
Ethan <stayhungry134@gmail.com>
2023-12-28 14:33:03 +0800

    feat: 显示其他文章

--- src/pages/reading/article.vue
+++ src/pages/reading/article.vue
@@ -1,15 +1,40 @@
 <script setup>
-import {onMounted, ref, reactive, computed} from "vue";
+import {onMounted, ref, reactive, computed, watch} from "vue";
 import {useColorStore} from "~/store/counter";
 import WordCard from "@/components/reading/word-card.vue";
-import {hexToRgb} from "~/assets/ts/utils";
+import {useRoute} from "vue-router";
+import ArticleItem from "@/components/reading/article-item.vue";
 
 /** 请求文章*/
+// 请求文章列表
 let article = reactive({})
-let api = '/word_api/reading/article/'
+let page_size = ref(10)
+let current_page = ref(1)
+const articles_page = ref(null)
+// 请求文章列表
+function get_article_list() {
+  fetch(`/word_api/reading/articles/?page=${current_page.value}&page_size=${page_size.value}`)
+      .then(response => response.json())
+      .then(data => {
+        articles_page.value = data
+      })
+      .catch(error => console.log(error))
+}
+// 改变每页大小
+function handle_current_change(val) {
+  current_page.value = val
+  get_article_list()
+}
 
-function get_article() {
-  fetch(api)
+// 请求文章
+let article_id = ref(null)
+// 获取路由
+const route = useRoute()
+function get_article(article_id=null) {
+  let url = '/word_api/reading/article/'
+  if (article_id)
+    url += `?id=${article_id}`
+  fetch(url)
       .then(response => response.json())
       .then(data => {
         Object.keys(data).forEach(key => {
@@ -20,8 +45,18 @@ function get_article() {
 }
 
 onMounted(() => {
+  // 获取路由中的id
+  if (route.query.id)
+    article_id.value = route.query.id
+  // 请求文章列表
+  get_article_list()
+  // 请求文章
   get_article()
-  console.log(article)
+})
+
+// 监听article_id的变化
+watch(article_id, (new_article_id) => {
+  get_article(new_article_id)
 })
 
 
@@ -68,16 +103,24 @@ const show_word_card = (e, word) => {
     <div ref="current_article" class="col-8  position-relative">
 <!--      文章标题-->
       <div class="title">
-        <h2 class="h2">{{ article.title }}</h2>
+        <h1 class="h1">{{ article.title_en }}</h1>
+        <h4 class="h4">{{ article.title_cn }}</h4>
       </div>
 <!--      创建、阅读日期-->
       <div class="date d-flex">
-        <div class="col-4">
-          创建日期：<span class="text-info">{{ article.init_date }}</span>
+        <div class="col-5">
+          创建日期：<span class="text-info">{{ article.create_time }}</span>
         </div>
-        <div class="col-4">
+        <div class="col-5">
           上次阅读日期：<span class="text-info">{{ article.last_review }}</span>
         </div>
+        <div class="col-2">
+          单词数：<span class="text-info">{{ article.length }}</span>
+        </div>
+      </div>
+<!--      文章图片-->
+      <div class="image">
+        <img :src="article.image" alt="">
       </div>
 <!--      文章内容-->
       <div class="content">
@@ -102,8 +145,28 @@ const show_word_card = (e, word) => {
                  :word="word_card.word"></word-card>
     </div>
     <!--    其他文章-->
-    <div class="col-4 ps-4">
-      其他文章
+    <div v-if="articles_page" class="col-4 ps-4">
+      <h2 class="h2">其他文章</h2>
+      <div class="article_list">
+        <div v-for="article in articles_page.articles"
+             :key="article.id"
+             class="mb-2">
+          <article-item :article="article"></article-item>
+        </div>
+      </div>
+
+      <!--  分页-->
+      <div class="d-flex container justify-content-end">
+        <el-pagination
+            class="mt-3"
+            v-model:current-page="current_page"
+            background
+            layout="total, prev, pager, next"
+            :total="articles_page.total"
+            page-count="5"
+            @current-change="handle_current_change"
+        />
+      </div>
     </div>
   </div>
 </template>
