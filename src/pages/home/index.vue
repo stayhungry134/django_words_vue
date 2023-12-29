01a4445d92a12c9983917dcaea06f59e8d141a80
Ethan <stayhungry134@gmail.com>
2023-12-29 16:38:11 +0800

    feat: 调整路由

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,14 +4,14 @@
 
 <template>
   <div class="container d-flex justify-content-around mx-auto row">
-      <div class="card col-3 m-5 p-0" @click="$router.push('/words/ebbinghaus')">
+      <div class="card col-3 m-5 p-0" @click="$router.push('/word/')">
           <img src="../../assets/image/word.jpg" class="card-img-top" alt="...">
           <div class="card-body h2 text-center">
               单词
           </div>
       </div>
-      <div class="card col-3 m-5 p-0">
-        <img src="../../assets/image/article.jpg" class="card-img-top" alt="...">
+      <div class="card col-3 m-5 p-0"  @click="$router.push('/reading/')">
+        <img src="../../assets/image/article.jpg" class="card-img-top" alt=". ..">
         <div class="card-body h2 text-center">
             读
         </div>
