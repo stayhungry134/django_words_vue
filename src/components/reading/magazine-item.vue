c8d15f019580560565d82b8e476da55e13a370c7
Ethan <stayhungry134@gmail.com>
2024-01-14 00:27:06 +0800

    feat: 完善杂志页面显示

--- src/components/reading/magazine-item.vue
+++ src/components/reading/magazine-item.vue
@@ -1,11 +1,14 @@
 <script setup>
 import {Delete, Download, Edit, Share} from "@element-plus/icons-vue";
-import {onMounted, ref, reactive, computed, watch} from "vue";
+import {onMounted, ref, reactive, computed, watch, defineProps} from "vue";
+import {ElMessage} from "element-plus";
 
-let magazine = reactive({
-  id: 1,
-  name: "DZWZ2018-07-08",
-  image: "http://word.stayhungry134.com:8000/word_media/magazine/RD201801.jpg",
+let props = defineProps({
+  magazine: {
+    type: Object,
+    default: () => {
+    }
+  }
 })
 // 图片查看器
 let show_image_viewer = ref(false)
@@ -16,39 +19,61 @@ const close_viewer = () => {
   show_image_viewer.value = false
 }
 
+// 下载文件
+const download_file = () => {
+  if (!props.magazine.local_path) {
+    ElMessage({
+      message: '该杂志暂时无法下载',
+      type: 'warning'
+    })
+    return
+  }
+  fetch(props.magazine.local_path).then(response => {
+    return response.blob()
+  }).then(blob => {
+    const url = window.URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = props.magazine.name
+    a.click()
+    window.URL.revokeObjectURL(url)
+  })
+}
+
 </script>
 
 <template>
-<div class="magazine-item">
-  <el-card :body-style="{ padding: '0px' }">
-    <el-image class="image"
-              :src="magazine.image"
-              fit="cover"
-              :preview-src-list="[magazine.image]"></el-image>
-    <div class="p-2">
-      <span>{{ magazine.name }}</span>
-      <div class="bottom">
-        <div class="d-flex mt-2 justify-content-end">
-          <el-tooltip content="下载">
-            <el-button :icon="Download" />
-          </el-tooltip>
-          <el-tooltip content="分享">
-            <el-button :icon="Share" />
-          </el-tooltip>
-          <el-button type="primary">阅读</el-button>
+  <div v-if="magazine" class="magazine-item mx-4 mb-5">
+    <el-card :body-style="{ padding: '0px' }">
+      <el-image class="image"
+                :src="magazine.cover"
+                fit="cover"
+                :preview-src-list="[magazine.cover]"></el-image>
+      <div class="p-2">
+        <span>{{ magazine.name }}</span>
+        <div class="bottom">
+          <div class="d-flex mt-2 justify-content-end">
+            <el-tooltip content="下载">
+              <el-button :icon="Download" @click="download_file"/>
+            </el-tooltip>
+            <el-tooltip content="分享">
+              <el-button :icon="Share"/>
+            </el-tooltip>
+            <el-button type="primary">阅读</el-button>
+          </div>
         </div>
       </div>
-    </div>
-  </el-card>
+    </el-card>
 
-  <el-image-viewer @close="close_viewer" :url-list="[magazine.image]" v-if="show_image_viewer" />
-</div>
+    <el-image-viewer @close="close_viewer" :url-list="[magazine.image]" v-if="show_image_viewer"/>
+  </div>
 </template>
 
 <style scoped lang="scss">
-.magazine-item{
+.magazine-item {
   width: 250px;
-  .image{
+
+  .image {
   }
 }
 </style>
--- src/pages/reading/magazine-tab.vue
+++ src/pages/reading/magazine-tab.vue
@@ -1,14 +1,79 @@
 <script setup>
-
+import {onMounted, ref, watch} from "vue";
 import MagazineItem from "@/components/reading/magazine-item.vue";
+
+/** 请求杂志类 */
+let categories = ref([])
+let current_category = ref(null)
+// 请求杂志分类
+const get_categories = async () => {
+  let res = await fetch(`/word_api/reading/category?classify=magazine`)
+      .then(response => response.json())
+  categories.value = res.items
+  current_category.value = categories.value[0].id
+  console.log(current_category.value)
+}
+watch(current_category, (new_val) => {
+  magazines.value = []
+  get_magazines(new_val)
+})
+
+let magazines = ref([])
+let magazine_pager = ref(null)
+// 请求杂志
+const get_magazines = async (category=null) => {
+  let url_api = `/word_api/reading/magazine?category_id=${category}`
+  if (category === null){
+    url_api = `/word_api/reading/magazine`
+  }
+  magazine_pager = await fetch(url_api)
+      .then(response => response.json())
+  magazine_pager.items.forEach(item => {
+    magazines.value.push(item)
+  })
+}
+
+onMounted(() => {
+  get_categories()
+  get_magazines()
+})
+
+
 </script>
 
 <template>
-  <div>
-    <magazine-item></magazine-item>
+  <div class="container row">
+    <div class="col-xl-9 col-12 d-flex flex-wrap justify-content-between">
+      <magazine-item v-for="magazine in magazines"
+                     :key="magazine.id"
+                     :magazine="magazine">
+      </magazine-item>
+    </div>
+    <div v-if="categories.length" class="col-xl-3 d-none d-xl-block">
+      <div class="h3">分类</div>
+      <div>
+        <nav class="nav flex-column">
+          <div v-for="category in categories"
+               :key="category.id"
+               class="category-item my-2 ps-2"
+               @click="current_category = category.id"
+               :class="{'active': current_category === category.id}">
+            {{ category.name }}
+          </div>
+        </nav>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style scoped lang="scss">
-
+.category-item{
+  cursor: pointer;
+  color: #303133;
+}
+.category-item.active{
+  color: #409eff;
+  border-left: 2px solid #409eff;
+}
 </style>
