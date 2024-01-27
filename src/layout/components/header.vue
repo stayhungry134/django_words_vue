b10097b6b6f3afd5cb90721cc4d7ccfb37df6c76
Ethan <stayhungry134@gmail.com>
2024-01-27 15:39:01 +0800

    style: 调整header z-index 层级

--- src/layout/components/header.vue
+++ src/layout/components/header.vue
@@ -11,7 +11,7 @@ function change_theme(item) {
 </script>
 
 <template>
-  <div class="header container-fluid d-none d-xl-block mb-5 position-fixed top-0 start-0 z-3" :style="{backgroundColor: color.theme_color, color: color.tab_color}">
+  <div class="header container-fluid d-none d-xl-block mb-5 position-fixed top-0 start-0" :style="{backgroundColor: color.theme_color, color: color.tab_color}">
     <div class="container h-100 d-flex justify-content-between align-items-center">
       <ul class="tab row nav nav-pills w-75 h-100">
         <li class="col-1 nav-item h-100 p-0 text-center" @click="$router.push('/')">Home</li>
@@ -45,6 +45,7 @@ function change_theme(item) {
 .header {
   height: 50px;
   font-style: italic;
+  z-index: 900;
   .tab{
 
     li {
