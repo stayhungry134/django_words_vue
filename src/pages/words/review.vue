8c8d63fe49dd558b0f69a22c0140668da5e4bccb
Ethan <stayhungry134@gmail.com>
2024-05-11 09:31:43 +0800

    fix: 单词第一次记忆的时候显示取消默写模式

--- src/pages/words/review.vue
+++ src/pages/words/review.vue
@@ -12,7 +12,7 @@ const fold_menu = () =>{
   menu_fold.value = !menu_fold.value
 }
 // 是否为默写模式
-let is_typing = ref(true)
+let is_typing = ref(false)
 const change_typing = () => {
   is_typing.value = !is_typing.value
 }
@@ -60,6 +60,13 @@ const next_word = () =>{
     now_word.value = null
   }
   now_word.value = word_list.value[Math.floor(Math.random() * word_list.value.length)].word
+  // 如果单词是第一次记忆，就显示原单词
+  if (word_memory_map.value[now_word.value.word] === 0){
+    is_typing.value = false
+  }
+  else {
+    is_typing.value = true
+  }
   // 如果小于5个还有下一页的话就获取单词
   if (word_list.value.length <= 5 && word_pager.value.has_next){
     get_word()
